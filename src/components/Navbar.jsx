498a9e2cb2ab19e41dbef5aed41b7587dfbd7801
deneshwar <deneshwarbhandarkar@gmail.com>
2024-05-18 17:54:48 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
 
-                    <Link to="./" class="navbar-brand" >Book Managment System</Link>
+                    <Link to="/" class="navbar-brand" >Book Managment System</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
